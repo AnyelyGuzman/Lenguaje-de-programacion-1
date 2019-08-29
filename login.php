50737bb388de49ff3caae12da6b931de01f15d3d
Anyely Guzmán <anyely_guzmanch@fet.edu.co>
2019-08-28 22:43:00 -0500

    pricipal

--- login.php
+++ login.php
@@ -50,6 +50,9 @@
 							<div class="form-group">
 								<button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal">Administrador</button>
 							</div>
+							<div class="form-group">
+								<button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModalRegistro">Registro</button>
+							</div>
 						</form>
 						<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 							<div class="modal-dialog" role="document">
@@ -77,6 +80,96 @@
 													</div>
 													<input class="form-control" placeholder="Contraseña" type="password">
 												</div>
+											</div>
+											<div class="form-group">
+												<button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+											</div>
+										</form>
+     								</div>
+      								<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      								</div>
+								</div>
+							</div>
+						</div>
+						<div class="modal fade" id="exampleModalRegistro" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="exampleModalLabel">Registro</h5>
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+      								<div class="modal-body">
+									  	<form action="ejemplo.php" method="get">
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-prepend">
+														<span class="input-group-text"> <i class="fa fa-user"></i> </span>
+													</div>
+													<input name="" class="form-control" placeholder="Nombre" type="text">
+												</div>
+											</div>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-prepend">
+														<span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+													</div>
+													<input class="form-control" placeholder="Cooreo" type="email">
+												</div>
+											</div>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-prepend">
+														<span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+													</div>
+													<input class="form-control" placeholder="Edad" type="number">
+												</div>
+											</div>
+											<div class="form-group">
+												<label for="genero">Genero</label>
+												<select class="form-control" id="genero">
+													<option value="Amazonas">Masculino</option>
+													<option value="Antioquia">Femenino</option>
+												</select>
+											</div>
+											<div class="form-group">
+												<label for="departamento">Departamentos</label>
+												<select class="form-control" id="departamento">
+													<option value="Amazonas">Amazonas</option>
+													<option value="Antioquia">Antioquia</option>
+													<option value="Arauca">Arauca</option>	
+													<option value="Atlantico">Atlantico</option>
+													<option value="Bolivar">Bolivar</option>
+													<option value="Boyaca">Boyaca</option>			
+													<option value="Caldas">Caldas</option>
+													<option value="Caqueta">Caqueta</option>
+													<option value="Casanare">Casanare</option>
+													<option value="Cauca">Cauca</option>
+													<option value="Cesar">Cesar</option>
+													<option value="Choco">Choco</option>
+													<option value="Cordoba">Cordoba</option>
+													<option value="Cundinamarca">Cundinamarca</option>
+													<option value="Guainia">Guainia</option>
+													<option value="Guaviare">Guaviare</option>
+													<option value="Huila">Huila</option>
+													<option value="Guajira">Guajira</option>
+													<option value="Magdalena">Magdalena</option>
+													<option value="Meta">Meta</option>
+													<option value="Nariño">Nariño</option>
+													<option value="Norte de Santander">Norte de Santander</option>
+													<option value="Putumayo">Putumayo</option>
+													<option value="Quindio">Quindio</option>
+													<option value="Risaralda">Risaralda</option>
+													<option value="San Andres">San Andres</option>
+													<option value="Santander">Santander</option>
+													<option value="Sucre">Sucre</option>
+													<option value="Tolima">Tolima</option>
+													<option value="Valle del Cauca">Valle del Cauca</option>
+													<option value="Vaupes">Vaupes</option>
+													<option value="Vichada">Vichada</option>
+												</select>
 											</div>
 											<div class="form-group">
 												<button type="submit" class="btn btn-primary btn-block">Ingresar</button>
